df76b651019b02e20b160e6fb204dd3062c45107
subreax <extode@gmail.com>
2023-04-05 19:30:13 +0300

    AlphaPicker slider bugfix

--- app/src/main/java/com/subreax/lightclient/ui/colorpicker/AlphaPicker.kt
+++ app/src/main/java/com/subreax/lightclient/ui/colorpicker/AlphaPicker.kt
@@ -39,7 +39,7 @@ fun AlphaPicker(
                     Brush.horizontalGradient(
                         listOf(
                             Color.Transparent,
-                            hsv.toColor()
+                            hsv.toColor(alpha = 1.0f)
                         )
                     )
                 ),
